037a7bcec734fd28b1607356ad49cc26b1aded37
unava1lable <910654119@qq.com>
2022-06-21 22:33:30 +0800

    implement Todo structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::collections::HashMap;
+
+struct Todo {
+    map: HashMap<String, bool>
+}
+
+impl Todo {
+    fn new() -> Self {
+        Self {
+            map: HashMap::new(),
+        }
+    }
+
+    fn insert(&mut self, key: String) {
+        self.map.insert(key, false);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut todo = Todo::new();
+    todo.insert("Exam".to_string());
+    println!("{:?}", todo.map);
 }
