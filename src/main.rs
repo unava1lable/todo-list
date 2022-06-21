a406ab72b16339ee53767f5c8b16979b19e2b8f8
unava1lable <910654119@qq.com>
2022-06-21 22:41:23 +0800

    add method save for Todo

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::fs;
 
 struct Todo {
     map: HashMap<String, bool>
@@ -20,10 +21,22 @@ impl Todo {
             println!("{} - {}", key, value);
         }
     }
+
+    fn save(&self) -> std::io::Result<()> {
+        let mut contents = String::new();
+        for (key, value) in self.map.iter() {
+            let record = format!("{}-{}\n", key, value);
+            contents.push_str(record.as_str());
+        }
+        fs::write("todo.txt", contents)?;
+
+        Ok(())
+    }
 }
 
 fn main() {
     let mut todo = Todo::new();
     todo.insert("Exam".to_string());
     todo.list();
+    todo.save();
 }
